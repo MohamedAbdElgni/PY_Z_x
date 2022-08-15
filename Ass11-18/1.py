cd5dd4835c3c6f33b473865f63a6dc311eb2b2fb
bismohamed <45873305+bismohamed@users.noreply.github.com>
2022-08-15 23:30:37 +0200

    Co-authored-by: bismohamed <45873305+bismohamed@users.noreply.github.com>

--- Ass11-18/1.py
+++ Ass11-18/1.py
@@ -4,39 +4,64 @@ Age = "38"
 Country = "Egypt"
 print(
     f'''Hello '{Name}', How You Doing \\"""" Your Age Is "{Age}"" + And Your Country Is: {Country}''')
-print("#"*25)
+print("="*25)
 # Assignment 2
 print(f'''Hello '{Name}', How You Doing \\\n"""" Your Age Is "{Age}""+\nAnd Your Country Is: {Country}''')
-print("#"*25)
+print("="*25)
 # Assignment 3
 name = 'Elzero'
 print(f"FIrst Letter is {name[1]}")
 print(f"Second Letter is {name[2]}")
 print(f"Third Letter is {name[3]}")
-print("#"*25)
+print("="*25)
 # Assignment 4
 print(f"FIrst Letter is {name[1:4]}")
 print(f"FIrst Letter is {name[0:5:2]}")
 print(f"FIrst Letter is {name[-2:-7:-2]}")
-print("#"*25)
+print("="*25)
 # Assignment 5
 naame = "#@#@Elzero#@#@"
 print(naame[4:10])
-print("#"*25)
+print("="*25)
 # Assignment 6
 num1 = "9"
 num2 = "15"
 num3 = "130"
 num4 = "950"
 num5 = "1500"
-print(num1.zfill(4, 0))
-print(num2.zfill(4, 0))
-print(num3.zfill(4, 0))
-print(num4.zfill(4, 0))
-print(num5.zfill(4, 0))
-print("#"*25)
-# Assignment7
+print(num1.zfill(4))
+print(num2.zfill(4))
+print(num3.zfill(4))
+print(num4.zfill(4))
+print(num5.zfill(4))
+print("="*25)
+# Assignment 7
 name_one = "Osama"
 name_two = "Osama_Elzero"
-print(name_one.zfill(20, "@"))
-print(name_two.zfill(20, "@"))
+print(name_one.rjust(20, "@"))
+print(name_two.rjust(20, "@"))
+print("="*25)
+# Assignment 8
+name_O = "OSamA"
+name_T = "osaMA"
+print(name_O.swapcase())
+print(name_T.swapcase())
+print("="*25)
+# Assignment 9
+msg = "I Love Python And Although Love Elzero Web School"
+print(msg.count("Love"))
+print("="*25)
+# Assignment 10
+namezz = "Elzero"
+print(namezz.index("z"))
+print("="*25)
+# Assignment 11
+msg2 = "I <3 Python And Although <3 Elzero Web School"
+print(msg2.replace("<3", "Love", 1))
+print("="*25)
+# Assignment 12
+print(msg2.replace("<3", "Love"))
+print("="*25)
+# Assignment 13
+print(
+    f"My Name Is {Name}, And My Age Is {int(Age)}, And My Country Is {Country}")
