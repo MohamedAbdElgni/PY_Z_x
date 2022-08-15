54028ffb567099d4bb0bd4e9f7d2179948d4d48e
bismohamed <45873305+bismohamed@users.noreply.github.com>
2022-08-15 21:38:39 +0200

    1-10 SOul

---
+++ ass1-10/1.py
@@ -0,0 +1,15 @@
+# Assignment 1,2
+Name = "Osama"
+Age = "38"
+Country = "Egypt"
+# Assignment 3
+print("Name: ", Name)
+print("Age: ", Age)
+print("Country: ", Country)
+# Assignment 4
+print(
+    f"Hello, My Name Is {Name} And Iam {Age} Years Old and I Live in {Country}.")
+# Assignment 5
+print(type(Name))
+print(type(Age))
+print(type(Country))
