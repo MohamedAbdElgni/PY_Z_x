7fe4037b6e2dbce45dd3df65b3bcc57c0b29dfc8
bismohamed <45873305+bismohamed@users.noreply.github.com>
2022-08-15 22:50:03 +0200

    11-18 Added

---
+++ Ass11-18/1.py
@@ -0,0 +1,42 @@
+# Assignment 1
+Name = "Osama"
+Age = "38"
+Country = "Egypt"
+print(
+    f'''Hello '{Name}', How You Doing \\"""" Your Age Is "{Age}"" + And Your Country Is: {Country}''')
+print("#"*25)
+# Assignment 2
+print(f'''Hello '{Name}', How You Doing \\\n"""" Your Age Is "{Age}""+\nAnd Your Country Is: {Country}''')
+print("#"*25)
+# Assignment 3
+name = 'Elzero'
+print(f"FIrst Letter is {name[1]}")
+print(f"Second Letter is {name[2]}")
+print(f"Third Letter is {name[3]}")
+print("#"*25)
+# Assignment 4
+print(f"FIrst Letter is {name[1:4]}")
+print(f"FIrst Letter is {name[0:5:2]}")
+print(f"FIrst Letter is {name[-2:-7:-2]}")
+print("#"*25)
+# Assignment 5
+naame = "#@#@Elzero#@#@"
+print(naame[4:10])
+print("#"*25)
+# Assignment 6
+num1 = "9"
+num2 = "15"
+num3 = "130"
+num4 = "950"
+num5 = "1500"
+print(num1.zfill(4, 0))
+print(num2.zfill(4, 0))
+print(num3.zfill(4, 0))
+print(num4.zfill(4, 0))
+print(num5.zfill(4, 0))
+print("#"*25)
+# Assignment7
+name_one = "Osama"
+name_two = "Osama_Elzero"
+print(name_one.zfill(20, "@"))
+print(name_two.zfill(20, "@"))
