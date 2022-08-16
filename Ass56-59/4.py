c137ad26e0dc7b9b79c2a2d6da989cb1f22fac86
bismohamed <45873305+bismohamed@users.noreply.github.com>
2022-08-16 09:54:34 +0200

    Edite

--- Ass56-59/4.py
+++ Ass56-59/4.py
@@ -2,5 +2,9 @@ def say_hello(name="unknown", age="unknown", country="unknown"):
     print(f"hello {name} your age is {age} and you live in {country}".title())
 
 
-say_hello("Osama", 38, "Egypt")
+name = input("Enter your name ==> ".title())
+age = input("Enter your age ==> ".title())
+country = input("Whats your country ==> ".title())
+
+say_hello(name, age, country)
 say_hello()
